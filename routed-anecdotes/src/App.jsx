17e63d89506377224230c1df4f088f4830abaefa
djanyreason <djanyreason@gmail.com>
2023-10-02 09:37:08 -0400

    Completes ex 7.1, adds routing to anecdotes app
    This commit uses React Router to add routing to the anecdotes
    application. It separates the anecdotes list, creation form, and
    about page into three separate routes, accessed at '/', '/create',
    and '/about' respectively.

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 const Menu = () => {
   const padding = {
@@ -7,9 +13,9 @@ const Menu = () => {
 
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to='/'>anecdotes</Link>
+      <Link style={padding} to='create'>create new</Link>
+      <Link style={padding} to='about'>about</Link>
     </div>
   );
 };
@@ -41,7 +47,9 @@ const Footer = () => (
   <div>
     Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
 
-    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+    See <a href='https://github.com/djanyreason/FSO_07/tree/main/routed-anecdotes/src'>
+      https://github.com/djanyreason/FSO_07/tree/main/routed-anecdotes/src
+    </a> for the source code.
   </div>
 );
 
@@ -79,6 +87,7 @@ const CreateNew = (props) => {
         </div>
         <button>create</button>
       </form>
+      <br />
     </div>
   );
 };
@@ -123,14 +132,16 @@ const App = () => {
   };
 
   return (
-    <div>
+    <Router>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/create' element={<CreateNew addNew={addNew} />} />
+        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
